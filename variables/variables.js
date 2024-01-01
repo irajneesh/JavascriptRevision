df83ee626c98806f5ca64742383f92d9d5243af3
irajneesh.kr@gmail.com <irajneesh.kr@gmail.com>
2024-01-01 18:47:58 +0530

    javascript variables

---
+++ variables/variables.js
@@ -0,0 +1,8 @@
+//var let and const
+//using camelCase writing
+const myId=1 //can't be changed,not allowed
+let email="irajneesh@gmail.com" //can be changed.
+var password="12345" //can be changed,global scope.
+// prefer not to use var , issue of block scope and functional scope.
+city ="jaipur"
+console.table([myId,email,city,password]);
