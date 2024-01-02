b214121f47e043fc001b502d22398cfa08ae0101
irajneesh.kr@gmail.com <irajneesh.kr@gmail.com>
2024-01-02 16:55:39 +0530

    conversion in js

---
+++ conversion/conversion.js
@@ -0,0 +1,40 @@
+//  let score = 33
+
+//  console.log(typeof score) //number
+ 
+ //string to number
+//  let score = "33"
+
+//  let value = Number(score); //changes to number.
+//  console.log(typeof score);
+//  console.log(typeof value);
+
+//"33abc" => NaN
+// true => 1, 1 =>true
+// false => 0, 0=>false
+
+// let isLoggedIn = 1
+
+// let booleanValue = Boolean(isLoggedIn)
+// console.log(booleanValue)
+
+// ****************Operations**************
+// string concatenation
+
+let str1 ="Rajneesh"
+let str2 ="singh"
+
+console.log(str1+str2)
+
+// brainDrain
+
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2");
+
+//  Ecma docs 7.1.1 ToPrimitive.
+
+// increment , decrement , prefix , postfix. =>read mdn docs.
+
+
+
