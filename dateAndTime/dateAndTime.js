19cf3761b9fe3927cfcd5233f420d0ea3ee1a2c3
irajneesh.kr@gmail.com <irajneesh.kr@gmail.com>
2024-01-04 23:01:31 +0530

    date and time in js

---
+++ dateAndTime/dateAndTime.js
@@ -0,0 +1,26 @@
+ //date 
+
+ let myDate = new Date()
+//  console.log(myDate.toString()); // Fri Oct 06 2023 17:39:30 GMT+0530 (India Standard Time)
+//  console.log(myDate.toDateString()); // Fri Oct 06 2023
+
+// typeof myDate === object
+
+// let createDate =new Date()
+// read mdn
+//getTime method , Date.now method. getDay , getMonth method.
+
+//time stamp
+
+// let myTimestamp = Date.now();
+// console.log(myTimestamp);
+
+//method
+
+// console.log(myDate.getMonth());
+// console.log(`year is ${myDate.getFullYear()} and day is ${myDate.getDay()}`);
+
+
+// myDate.toLocaleString('default',{
+//     weekday: "long"
+// })
