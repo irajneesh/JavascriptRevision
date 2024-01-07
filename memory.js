056a67f4e713308079fdf762b9851be5620750ef
irajneesh.kr@gmail.com <irajneesh.kr@gmail.com>
2024-01-07 19:20:14 +0530

    memory in js

---
+++ memory.js
@@ -0,0 +1,9 @@
+// *********Memory**********
+
+// Stack(Primitive) Heap(Non-primitive) 
+
+// Heap => reference of original value , changes will happen in original value
+// stack => copy of variable
+
+
+
